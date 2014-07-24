8243e41c1633ed156c156243fed0719be8890e4a
José F. Romaniello <jfromaniello@gmail.com>
2014-07-24 12:44:12 -0300

    initial

---
+++ main.js
@@ -0,0 +1,11 @@
+function addGlobalStyle(css) {
+  var head, style;
+  head = document.getElementsByTagName('head')[0];
+  if (!head) { return; }
+  style = document.createElement('style');
+  style.type = 'text/css';
+  style.innerHTML = css;
+  head.appendChild(style);
+}
+
+addGlobalStyle('.file-box .line {tab-size: 4; -moz-tab-size: 4; -o-tab-size: 4; white-space: pre-wrap;}');
